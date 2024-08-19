34f277d4cc3cbc98ac9d1058d860c9cdaafbd015
ClarkG10 <dalautaclarkraven@gmail.com>
2024-08-19 12:24:59 +0800

    update users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('role')->default('admin');
             $table->string('password');
-            $table->string('fullname')->nullable();
-            $table->string('address')->nullable();
-            $table->string('phonenumber')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
